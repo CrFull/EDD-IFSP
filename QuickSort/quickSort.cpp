8b8b375b3bb33976d2cebae82dc63fd07ddda4c0
João Victor <126835516+CrFull@users.noreply.github.com>
2024-08-08 17:58:28 -0300

    Update  quickSort.cpp
    Putting the same numbers on the array of the last class as asked.

--- QuickSort/quickSort.cpp
+++ QuickSort/quickSort.cpp
@@ -1,42 +1,42 @@
 #include <iostream>
 using namespace std;
 
-// FunÁ„o para trocar dois elementos
+// Fun√ß√£o para trocar dois elementos
 void swap(int &a, int &b) {
     int temp = a;
     a = b;
     b = temp;
 }
 
-// FunÁ„o para encontrar o Ìndice do pivÙ
+// Fun√ß√£o para encontrar o √≠ndice do piv√¥
 int partition(int arr[], int low, int high) {
-    int pivot = arr[high]; // Escolhe o ˙ltimo elemento como pivÙ
-    int i = (low - 1); // Õndice do menor elemento
+    int pivot = arr[high]; // Escolhe o √∫ltimo elemento como piv√¥
+    int i = (low - 1); // √çndice do menor elemento
 
     for (int j = low; j < high; j++) {
-        // Se o elemento atual È menor ou igual ao pivÙ
+        // Se o elemento atual √© menor ou igual ao piv√¥
         if (arr[j] <= pivot) {
-            i++; // Incrementa o Ìndice do menor elemento
+            i++; // Incrementa o √≠ndice do menor elemento
             swap(arr[i], arr[j]); // Troca os elementos
         }
     }
-    swap(arr[i + 1], arr[high]); // Coloca o pivÙ na posiÁ„o correta
+    swap(arr[i + 1], arr[high]); // Coloca o piv√¥ na posi√ß√£o correta
     return (i + 1);
 }
 
-// FunÁ„o recursiva de Quick Sort
+// Fun√ß√£o recursiva de Quick Sort
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
-        // Encontra o Ìndice do pivÙ, tal que todos os elementos menores que o pivÙ est„o ‡ sua esquerda e os maiores ‡ sua direita
+        // Encontra o √≠ndice do piv√¥, tal que todos os elementos menores que o piv√¥ est√£o √† sua esquerda e os maiores √† sua direita
         int pi = partition(arr, low, high);
 
-        // Ordena os elementos antes e depois do pivÙ
+        // Ordena os elementos antes e depois do piv√¥
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
 
-// FunÁ„o para imprimir o array
+// Fun√ß√£o para imprimir o array
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++)
         cout << arr[i] << " ";
@@ -44,15 +44,15 @@ void printArray(int arr[], int size) {
 }
 
 int main() {
-    int arr[] = {10, 7, 8, 9, 1, 5,100,0,1,-1,100,200,300,400,100};
+    int arr[] = {49, 38, 58, 87, 34, 93, 26, 13};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    cout << "Array antes da ordenaÁ„o: ";
+    cout << "Array antes da ordena√ß√£o: ";
     printArray(arr, n);
 
     quickSort(arr, 0, n - 1);
 
-    cout << "Array depois da ordenaÁ„o: ";
+    cout << "Array depois da ordena√ß√£o: ";
     printArray(arr, n);
     
     return 0;
