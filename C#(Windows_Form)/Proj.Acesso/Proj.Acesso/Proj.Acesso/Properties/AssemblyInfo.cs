ba913a48d026c68e9f9601114c0ccb3df4ca1fc5
João Victor <joaocrivoi13@gmail.com>
2024-12-09 19:39:53 -0300

    Adding the Project Acesso first codes

---
+++ C#(Windows_Form)/Proj.Acesso/Proj.Acesso/Proj.Acesso/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Proj.Acesso")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Proj.Acesso")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("a2a72b7b-7c61-48cf-aa59-1e0d2fa0a9fe")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ C#(Windows_Form)/Proj.Acesso/Proj.Acesso/Proj.Acesso/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj.Acesso
+{
+    class Usuario
+    {
+        private int id;
+        private string name;
+        private List<Ambiente> ambientes;
+
+        public Usuario(int id, string name, List<Ambiente> ambientes)
+        {
+            this.id = id;
+            this.name = name;
+            this.ambientes = ambientes;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        internal List<Ambiente> Ambientes { get => ambientes; set => ambientes = value; }
+        
+        public bool concederPermissao(Ambiente ambiente)
+        {
+
+        }
+        public bool revogarPermissao(Ambiente ambiente)
+        {
+
+        }
+    }
+}
